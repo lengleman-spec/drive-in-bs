799fce9a7c6e2e7a303c8896fe09e110bee19183
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-04-20 12:44:14 -0400

    Logic for fadeIn animation for 'coming soon' cards upon scroll.

---
+++ script.js
@@ -0,0 +1,17 @@
+const cards = document.querySelectorAll(".coming-soon-container .reveal-card");
+
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
+
+cards.forEach((card) => observer.observe(card));
