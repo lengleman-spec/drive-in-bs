0cebd83f9670074b205d2fe88ef93ecc3c791093
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-04-20 14:31:50 -0400

    Add 'admission & pricing section w/ animated title. Add cards.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const cards = document.querySelectorAll(".coming-soon-container .reveal-card");
+const pricingTitle = document.querySelector(
+  ".pricing-container .pricing-title-reveal",
+);
 
 const observer = new IntersectionObserver(
   (entries, observer) => {
@@ -15,3 +18,19 @@ const observer = new IntersectionObserver(
 );
 
 cards.forEach((card) => observer.observe(card));
+
+const titleObserver = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+  },
+);
+
+titleObserver.observe(pricingTitle);
